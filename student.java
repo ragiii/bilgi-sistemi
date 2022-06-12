d0963f828e816bab1d251694cfe6696332ab46ca
Sercan S <83725191+ragiii@users.noreply.github.com>
2022-06-12 20:38:06 +0300

    Create student.java

---
+++ student.java
@@ -0,0 +1,64 @@
+public class Student {
+    String name,stuNo;
+    int classes;
+    Course mat;
+    Course fizik;
+    Course kimya;
+    double avarage;
+    boolean isPass;
+
+
+    Student(String name, int classes, String stuNo, Course mat,Course fizik,Course kimya) {
+        this.name = name;
+        this.classes = classes;
+        this.stuNo = stuNo;
+        this.mat = mat;
+        this.fizik = fizik;
+        this.kimya = kimya;
+        calcAvarage();
+        this.isPass = false;
+    }
+    
+    private void calcAvarage() {
+    }
+
+    public void addBulkExamNote(int mat, int fizik, int kimya) {
+
+        if (mat >= 0 && mat <= 100) {
+            this.mat.note = mat;
+        }
+
+        if (fizik >= 0 && fizik <= 100) {
+            this.fizik.note = fizik;
+        }
+
+        if (kimya >= 0 && kimya <= 100) {
+            this.kimya.note = kimya;
+        }
+
+    }
+
+    public void isPass() {
+        if (this.mat.note == 0 || this.fizik.note == 0 || this.kimya.note == 0) {
+            System.out.println("Notlar tam olarak girilmemiş");
+        } else {
+            this.isPass = isCheckPass();
+            printNote();
+            System.out.println("Ortalama : " + this.avarage);
+            if (this.isPass) {
+                System.out.println("Sınıfı Geçti. ");
+            } else {
+                System.out.println("Sınıfta Kaldı.");
+            }
+        }
+            
+    }
+
+    private boolean isCheckPass() {
+        return false;
+    }
+
+    private void printNote() {
+    }   
+    
+}
